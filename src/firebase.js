3fc4915368d01b01d602dd0d9b4d2e04b93164c0
farahkhan77766655-eng <farahkhan77766655@gmail.com>
2026-03-26 20:56:13 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"; // ← add this
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAgMhncW-5aatQYICwJ0vNgEClZPeZdmjs",
   authDomain: "lahore-race-club-e22b4.firebaseapp.com",
   databaseURL: "https://lahore-race-club-e22b4-default-rtdb.firebaseio.com",
   projectId: "lahore-race-club-e22b4",
-  storageBucket: "lahore-race-club-e22b4.firebasestorage.app",
+  storageBucket: "lahore-race-club-e22b4.appspot.com",
   messagingSenderId: "338332055579",
   appId: "1:338332055579:web:bc89c06380358c9310a0be"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and export it
 export const db = getDatabase(app);
