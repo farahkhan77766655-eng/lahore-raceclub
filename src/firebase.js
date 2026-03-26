98d3afa0387f506054b30c0859f4431af690497e
farahkhan77766655-eng <farahkhan77766655@gmail.com>
2026-03-26 18:24:40 +0000

    Remove databaseURL from Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, setDoc, doc, updateDoc } from "firebase/firestore";
 
@@ -6,16 +5,13 @@ import { getFirestore, collection, getDocs, setDoc, doc, updateDoc } from "fireb
 const firebaseConfig = {
   apiKey: "AIzaSyAgMhncW-5aatQYICwJ0vNgEClZPeZdmjs",
   authDomain: "lahore-race-club-e22b4.firebaseapp.com",
-  databaseURL: "https://lahore-race-club-e22b4-default-rtdb.firebaseio.com",
   projectId: "lahore-race-club-e22b4",
   storageBucket: "lahore-race-club-e22b4.firebasestorage.app",
   messagingSenderId: "338332055579",
   appId: "1:338332055579:web:bc89c06380358c9310a0be"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export Firestore functions
 export { db, collection, getDocs, setDoc, doc, updateDoc };
