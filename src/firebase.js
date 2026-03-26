d94fe5e43abb68037bf07535441fe9d0648a8b27
farahkhan77766655-eng <farahkhan77766655@gmail.com>
2026-03-26 20:34:39 +0000

    Import getDatabase and export db from firebase.js
    Added import for getDatabase and updated export statement.

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database"; // ← add this
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAgMhncW-5aatQYICwJ0vNgEClZPeZdmjs",
   authDomain: "lahore-race-club-e22b4.firebaseapp.com",
@@ -11,5 +13,8 @@ const firebaseConfig = {
   appId: "1:338332055579:web:bc89c06380358c9310a0be"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and export it
 export const db = getDatabase(app);
