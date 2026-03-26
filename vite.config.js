95e8d0fc548bf66466ed8b717ea46cf6bbc47794
farahkhan77766655-eng <farahkhan77766655@gmail.com>
2026-03-26 21:12:29 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',        // ✅ Add this line for Vercel deployment
   plugins: [react()]
 });
