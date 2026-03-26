611f44fb8b468ed4567548756523e3b6a4d896d9
farahkhan77766655-eng <farahkhan77766655@gmail.com>
2026-03-26 20:01:13 +0000

    Add firebase.js config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgMhncW-5aatQYICwJ0vNgEClZPeZdmjs",
+  authDomain: "lahore-race-club-e22b4.firebaseapp.com",
+  databaseURL: "https://lahore-race-club-e22b4-default-rtdb.firebaseio.com",
+  projectId: "lahore-race-club-e22b4",
+  storageBucket: "lahore-race-club-e22b4.firebasestorage.app",
+  messagingSenderId: "338332055579",
+  appId: "1:338332055579:web:bc89c06380358c9310a0be"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
